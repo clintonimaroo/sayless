5cb7a9f69aa02b41d972629f1025677acb614284
Clinton Imaro <imarooclinton@gmail.com>
2024-05-06 10:30:00 -0700

    docs: clarify performance usage

--- sayless/cli/ai_providers.py
+++ sayless/cli/ai_providers.py
@@ -103,3 +103,4 @@ class OpenAIProvider(AIProvider):
 # Docs update: logging - 2025-05-21 11:23
 # Fix update: testing - 2025-05-21 17:51
 # Refactor update: config - 2025-05-21 19:00
+# Docs update: performance - 2025-05-22 18:52
