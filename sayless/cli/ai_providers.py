27942454bb9636e8e2ff0867ee151681d17815a5
Teddy Oweh <imarooclinton@gmail.com>
2025-01-21 19:30:00 -0800

    refactor: optimize timeout performance

--- sayless/cli/ai_providers.py
+++ sayless/cli/ai_providers.py
@@ -116,3 +116,4 @@ class OpenAIProvider(AIProvider):
 # Refactor update: provider - 2025-05-20 16:04
 # Chore update: CLI - 2025-05-22 00:47
 # Chore update: timeout - 2025-05-22 15:18
+# Refactor update: timeout - 2025-05-23 12:24
