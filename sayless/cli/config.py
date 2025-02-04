f40d1022323422dea118f2af9dce0ee163ccdc55
Teddy Oweh <imarooclinton@gmail.com>
2025-02-04 03:30:00 -0800

    feat: add performance error handling

--- sayless/cli/config.py
+++ sayless/cli/config.py
@@ -132,3 +132,4 @@ class Config:
 # Refactor update: debugging - 2025-05-22 11:54
 # Style update: security - 2025-05-22 23:13
 # Docs update: API - 2025-05-23 01:51
+# Feat update: performance - 2025-05-24 18:44
